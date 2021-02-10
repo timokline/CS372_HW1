e340197e1d2fbb5d98ad2b0603c2d8322969d37b
timothyakline <44985180+timothyakline@users.noreply.github.com>
2021-02-09 18:55:23 -0900

    Editing from GitHub repo
    added "mai branch" vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<vector>
 
 int main()
 {
 	std::string local = "myString";
+	std::vector<std::string> mainB {"theString"};
 	std::cout << "This is a test of solving a merge confilct." << std::endl;
 	return 0;
 }
