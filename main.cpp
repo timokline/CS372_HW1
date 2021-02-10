44e5fdacd7b714a27f9de7896e1a353e41b53c65
timothyakline <44985180+timothyakline@users.noreply.github.com>
2021-02-09 19:03:06 -0900

    "fixing typo" from Github to invoke merge conflict
    the word "conflict" spelled "wrong" intentionally

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main()
 {
 	std::string local = "myString";
 	std::vector<std::string> mainB {"theString"};
-	std::cout << "This is a test of solving a merge confilct." << std::endl;
+	std::cout << "This is a test of solving a merge conflict." << std::endl;
 	return 0;
 }
